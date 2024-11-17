e80a879ebc0b631ac1b5fafd431fec4d4703f956
kavi castelo <kavindu123450@gmail.com>
2024-11-17 19:42:52 +0530

    game score

--- world.gd
+++ world.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
+@export var game_stats: GameStats
+
 @onready var ship: Node2D = $Ship
+@onready var score_label: Label = $ScoreLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	update_score_label(game_stats.score) 
+	game_stats.score_changed.connect(update_score_label)
+	
 	ship.tree_exiting.connect(func():
 		await get_tree().create_timer(1.0).timeout
 		get_tree().change_scene_to_file("res://menus/game_over.tscn")
@@ -13,3 +19,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func update_score_label(new_score: int) -> void:
+	score_label.text = "Score: " + str(new_score)
