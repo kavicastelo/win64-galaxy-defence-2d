4d9c042dc3a527d6d43b4139b8e08140fc0780a4
kavi castelo <kavindu123450@gmail.com>
2024-11-19 02:23:32 +0530

    export project

--- menus/game_over.gd
+++ menus/game_over.gd
@@ -21,7 +21,7 @@ func _ready() -> void:
 	high_score_value_label.text = str(game_stats.highscore)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if (Input.is_action_just_pressed("ui_accept")):
 		game_stats.score = 0
 		get_tree().change_scene_to_file("res://menus/menu.tscn")
